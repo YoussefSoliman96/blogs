dfd74c0f92533c8a5485cdd2b8edb24d2e3e733b
youssef soliman <youssef.soliman6636@gmail.com>
2024-04-13 13:18:57 +0200

    Style the active link in the NavBar

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBlog } from "react-icons/fa6";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   const links = [
-    { label: "Home", href: "/" },
+    { label: "Blogs", href: "/blogs" },
     { label: "Signin", href: "/signin" },
     { label: "Signout", href: "/signout" },
   ];
@@ -17,7 +23,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
